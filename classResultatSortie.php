3144d3c593303ecb256b7cf276f87c9ca4895608
ilaneBen <i.benazzouz@it-students.fr>
2024-06-10 13:26:09 +0200

    tp_ilaneBenFinal

--- classResultatSortie.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class ResultatSortie {
-    const SUCCESS = 1;
-    const FAILURE = 2;
-    const CRITICAL_SUCCESS = 3;
-    const FUMBLE = 4;
-}
--- python/Equipement.py
+++
@@ -1,9 +0,0 @@
-import Statistiques
-class Equipement:
-    def __init__(self, nom, modificateurs):
-        self.nom = nom
-        self.modificateurs = modificateurs
-
-    def get_modificateurs(self):
-        return self.modificateurs
-
